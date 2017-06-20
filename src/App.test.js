98037860186f7bf3fa2ce7233b7b120f5665e185
Andrew Noyes <anoyes34@gmail.com>
2017-06-20 09:55:43 -0700

    Mock axios requests for tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import moxios from 'moxios'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});
+
+describe('mocking axios requests', function () {
+
+  describe('across entire suite', function () {
+
+    beforeEach(function () {
+      // import and pass your custom axios instance to this method
+      moxios.install()
+    })
+
+    afterEach(function () {
+      // import and pass your custom axios instance to this method
+      moxios.uninstall()
+    })
+
+    it('renders without crashing', (done) => {
+      const div = document.createElement('div');
+      ReactDOM.render(<App />, div);
+
+      moxios.wait(function () {
+        let request = moxios.requests.mostRecent()
+        request.respondWith({
+          status: 200,
+          response: {
+            goals: [
+              {
+                lastDone: "2017-06-20T09:38:20.194657",
+                userid: 1,
+                goalid: 1,
+                name: "some goal"
+              }
+            ]
+          }
+        }).then(() => done())
+      })
+    });
+
+  })
+
+})
